ca336ee5da8c7e00a2fd87dcae5903ac98577287
Minha02 <fathumaminha@hmail.com>
2024-07-17 12:29:56 +0530

    Animated Welcome page on app.js

--- src/components/AnimatedLine.js
+++ src/components/AnimatedLine.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { useSpring, animated} from 'react-spring';
+import './AnimatedLine.css'
 
 const AnimatedLine = () => {
-    const props= useSpring({width: '100%', from:{ width:'0%'}});
-
-  return (
-    <div style={{width:'100%', height:'4px',background:'#e0e0e0'}}>
-      <animated.div style={{...props, height:'4px',background:'#3498db'}}/>
-    </div>
-  )
+    const styles = useSpring({
+        from: { width: '0%' },
+        to: { width: '100%' },
+        config: { duration: 2000 },  // Adjust the duration as needed
+      });
+    
+      return (
+        <div className="line-container">
+          <animated.div className="line" style={styles}></animated.div>
+        </div>
+      );
 }
 
 export default AnimatedLine
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,6 +1,7 @@
 import React,{ useState, useEffect} from 'react'
 import './Home.css'
 import AnimatedLine from '../AnimatedLine';
+import { useSpring, animated } from 'react-spring';
 
 const Home =() => {
   const [welcomeMessage,setWelcomeMessage] = useState('Welcome to ProFitness!');
@@ -13,16 +14,25 @@ const Home =() => {
     setWelcomeMessage(event.target.value);
   };
 
+  const textStyles = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 2000 },
+  });
+
+
   return (
     <div className='home'>
       <div className='welcome'>
       <AnimatedLine />
+      <animated.div style={textStyles} className="text-container">
       <h1 className='wh1'>WELCOME TO PROFITNESS</h1>
       <h2 className='wh2'>Your Journey to a Healthier, Happier You Starts Here</h2>
       <p className='wp'>At FitLife, we believe that everyone deserves the opportunity to achieve their fitness goals and live their best life. Whether you’re just beginning your fitness journey or looking to take your training to the next level, we’re here to support you every step of the way.
       </p><p className='wp'>
       Our mission is to empower you with the tools, knowledge, and community needed to transform your health and wellness. With personalized plans, expert advice, and a supportive environment, your goals are within reach.
       </p>
+      </animated.div>
     </div>
       <h1>{welcomeMessage}</h1>
       <input 
@@ -30,11 +40,7 @@ const Home =() => {
         placeholder='Update welcome message'
         onChange={handleInputChange}
       />
-      <p>asda
-        asdasd
-        asdasda
-        asdasda
-      </p>
+      
     </div>
   )
 }
